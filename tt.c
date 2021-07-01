6af66312403ffce50754e0a9bd043779a47e58a4
Justinas Grigas <jstn_as@protonmail.com>
2021-07-02 00:44:38 +0100

    simplified current window
    window.h was overengineered, using switch statements instead.

--- tt.c
+++ tt.c
@@ -2,27 +2,19 @@
 #include "config.h"
 #include "tmenu.h"
 #include "twindow.h"
-#include "window.h"
 #include "tdraw.h"
 #include "tresult.h"
 
-#define WINDOW_COUNT 3
-Window *windows[WINDOW_COUNT];
-TResult *test_result;
+static TResult *test_result;
 // TODO: resize the window when switching windows.
-size_t target_window = 1;
-int running = 1;
+static size_t current_window = 0;
+static bool running = true;
+// Windows.
+static TWindow *test_window;
+static TMenu *main_menu;
+static TMenu *results_menu;
 
-void draw();
-void destroy_windows();
-
-void m_update(void *menu);
-void m_draw(void *menu);
-void m_destroy(void *menu);
-
-void t_update(void *twindow);
-void t_draw(void *twindow);
-void t_destroy(void *twindow);
+void m_update(TMenu *);
 
 int main() {
 	// Init.
@@ -41,78 +33,76 @@ int main() {
 	*test_result = tresult_init(0, 0);
 	// Create windows.
 	char *menu_options[] = {"Next test", "Repeat test", "Exit", NULL};
-	TMenu *menu = tmenu_init("Menu", menu_options);
-	windows[0] = window_init(menu, m_update, m_draw, m_destroy);
-	TWindow *twindow = twindow_init(time(NULL), test_result);
-	windows[1] = window_init(twindow, t_update, t_draw, t_destroy);
-	TMenu *results_menu = tmenu_result_init("Results", menu_options, test_result);
-	windows[2] = window_init(results_menu, m_update, m_draw, m_destroy);
+	test_window = twindow_init(time(NULL), test_result);
+	main_menu = tmenu_init("Menu", menu_options);
+	results_menu = tmenu_result_init("Results", menu_options, test_result);
 	// Main loop.
 	while (running) {
 		// Draw.
-		draw();
-		// Update.
-		windows[target_window]->update(windows[target_window]->window);
+		clear();
+		mvprintw(0, 0, "tt");
+		refresh();
+		// Draw and update current window.
+		switch (current_window) {
+			// Test window.
+			case 0:
+				twindow_draw(test_window);
+				const int result = twindow_update(test_window);
+				switch (result) {
+					// Completed the test.
+					case 0:
+						current_window = 2;
+						break;
+					// Escape key pressed.
+					case -2:
+						current_window = 1;
+						break;
+				}
+				break;
+			// Main menu.
+			case 1:
+				tmenu_draw(main_menu);
+				m_update(main_menu);
+				break;
+			// Results menu.
+			case 2:
+				tmenu_draw(results_menu);
+				m_update(results_menu);
+				break;
+		}
 	}
-	// Exit.
+	// Cleanup.
 	endwin();
-	destroy_windows();
+	twindow_destroy(test_window);
+	tmenu_destroy(main_menu);
+	tmenu_destroy(results_menu);
 	return 0;
 }
 
-void draw() {
-	clear();
-	mvprintw(0, 0, "tt");
-	refresh();
-	windows[target_window]->draw(windows[target_window]->window);
-}
-
-void destroy_windows() {
-	for (size_t w = 0; w < WINDOW_COUNT; w++) {
-		windows[w]->destroy(windows[w]);
-	}
-}
-
 // Menu window functions.
-void m_update(void *menu) {
-	const int result = tmenu_update((TMenu *)menu);
-	// Restart.
-	if (result == 0) {
-		// Destroy twindow.
-		windows[1]->destroy(windows[1]);
-		// Create new twindow.
-		TWindow *twindow = twindow_init(time(NULL), test_result);
-		windows[1] = window_init(twindow, t_update, t_draw, t_destroy);
-		target_window = 1;
+void m_update(TMenu *menu) {
+	const int result = tmenu_update(menu);
+	switch (result) {
+		// Next test.
+		case 0:
+			twindow_destroy(test_window);
+			test_window = twindow_init(time(NULL), test_result);
+			current_window = 0;
+			break;
+		// Repeat test.
+		case 1:
+			const size_t seed = test_window->seed;
+			twindow_destroy(test_window);
+			test_window = twindow_init(seed, test_result);
+			current_window = 0;
+			break;
+		// Exit.
+		case 2:
+			running = false;
+			break;
+		// Escape key pressed.
+		case -2:
+			current_window = 0;
+			break;
 	}
-	else if (result == 1) {
-		const size_t seed = ((TWindow *)windows[1]->window)->seed;
-		windows[1]->destroy(windows[1]);
-		TWindow *new_window = twindow_init(seed, test_result);
-		windows[1] = window_init(new_window, t_update, t_draw, t_destroy);
-		target_window = 1;
-	}
-	// Exit.
-	else if (result == 2)
-		running = 0;
-	// Escape.
-	else if (result == -2)
-		target_window = 1;
 }
-
-void m_draw(void *menu) { tmenu_draw((TMenu *)menu); };
-void m_destroy(void *menu) { tmenu_destroy((TMenu *)menu); };
-
-// Typing window functions.
-void t_update(void *twindow) {
-	const int result = twindow_update((TWindow *)twindow);
-	// Completed the test.
-	if (result == 0)
-		target_window = 2;
-	// Escape.
-	else if (result == -2)
-		target_window = 0;
-}
-
-void t_draw(void *twindow) { twindow_draw((TWindow *)twindow); };
-void t_destroy(void *twindow) { twindow_destroy((TWindow *)twindow); };
